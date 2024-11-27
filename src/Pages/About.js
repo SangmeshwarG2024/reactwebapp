f7bf05edb87c705c5e641cfe50d7e9b3a49091c0
SangmeshwarG2024 <sangmeshwargundile@gmail.com>
2024-11-27 21:06:23 +0530

    infinite scrolling added

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,6 +1,11 @@
 import React from "react";
 const About = () => {
-  return <h1>About page</h1>;
+  return (
+    <>
+      <h1>About page</h1>
+      <p>This section have content soon</p>
+    </>
+  );
 };
 
 export default About;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,19 +1,47 @@
-import React, { useEffect, useState } from "react";
-import Card from "../components/Card";
+import React, { useEffect, useState, Suspense } from "react";
 
+const Card = React.lazy(() => import("../components/Card"));
 const Home = () => {
   const [data, setData] = useState([]);
+  const [pageIndex, setPageindex] = useState(1);
 
-  useEffect(() => {
-    fetch("https://dummyjson.com/products")
+  const fetchData = async () => {
+    await fetch(
+      `https://dummyjson.com/products?limit=12&skip=${pageIndex * 12}`
+    )
       .then((res) => res.json())
-      .then((res) => setData(res));
+      .then((res) => setData((prev) => [...prev, ...res?.products]));
+  };
+
+  //fetching data on pageIndex changed
+  useEffect(() => {
+    fetchData();
+  }, [pageIndex]);
+
+  // load data on scroll
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
   }, []);
-  console.log(data);
+
+  // on scroll logic
+  const handleScroll = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop + 1 >
+      document.documentElement.scrollHeight
+    ) {
+      setPageindex((prev) => prev + 1);
+    }
+  };
+  // console.log(data);
   return (
     <>
       <h1>Products</h1>
-      <Card products={data?.products} />
+      <Suspense fallback={<h1>Loading..............</h1>}>
+        <Card products={data} />
+      </Suspense>
     </>
   );
 };
